c9b12e7bb6c42388cf4f1b280918e34cbe68bf2a
march-dave <ysangy@gmail.com>
2018-11-15 21:11:38 -0500

    update fizzbuzz

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -15,7 +15,8 @@
 function fizzBuzz(n) {
     if ( n % 3 === 0  && n % 5 === 0) return "fizzbuzz";
     if ( n % 3 === 0 ) return "fizz";
-    if (n % 5 === 0 ) return "buzz"
+    if ( n % 5 === 0 ) return "buzz"
+    return n;
 }
 
 module.exports = fizzBuzz;
