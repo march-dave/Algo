d1ed5e384d9ccfc76c561f94ab60f81d076d3b76
march-dave <ysangy@gmail.com>
2018-11-15 21:11:00 -0500

    javascript practice

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,10 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+    if ( n % 3 === 0  && n % 5 === 0) return "fizzbuzz";
+    if ( n % 3 === 0 ) return "fizz";
+    if (n % 5 === 0 ) return "buzz"
+}
 
 module.exports = fizzBuzz;
