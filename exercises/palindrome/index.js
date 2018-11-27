3e206b2b3a1273b248f43406bb2299570ffb0774
march-dave <ysangy@gmail.com>
2018-11-26 19:45:44 -0500

    javascript practice

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,9 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+    var ret = str.split('').reverse().join('');
+    return (ret === str) ? true : false;
+}
 
 module.exports = palindrome;
